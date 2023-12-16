94bb9b0d85bf5162f252417290277edbb6d9ca70
QuinSDev <quindev97@gmail.com>
2023-12-16 17:06:28 -0500

    Se actualiza el responsive para celulares

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
 
   return (
     <div className="flex h-[75vh] sm:h-[88vh] py-12 sm:py-14 px-14 xl:px-32 lg:px-20 md:px-20 sm:px-16 justify-end component">
-      <div className="flex flex-col max-h-full xl:max-w-xl lg:max-w-md lg:justify-start md:justify-center sm:justify-center ">
+      <div className="flex flex-col max-h-full xl:max-w-xl lg:max-w-md lg:justify-start md:justify-center sm:justify-center justify-center">
         <h1 className="xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-3xl lg:text-left text-center font-bold text-white">Sebastian Quintero</h1>
         <h3 className="sm:mt-4 mt-1 lg:text-4xl md:text-3xl sm:text-2xl text-2xl lg:text-left text-center font-bold" style={secondColor}>FullStack Developer</h3>
         <p className="sm:text-xl text-lg lg:text-left text-center sm:mt-8 mt-5 text-white">Como apasionado desarrollador FullStack, traduzco conceptos creativos
