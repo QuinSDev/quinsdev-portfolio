e6c33cb2d3f4e65d86b209c86b7e38bf6569ec01
quinsdev <quindev97@gmail.com>
2026-05-28 20:49:29 -0500

    docs: actualizar perfil profesional enfocado en desarrollo backend en el Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,12 +16,9 @@ export const Hero = () => {
     <div className="flex h-[75vh] sm:h-[88vh] py-12 sm:py-14 px-14 xl:px-32 lg:px-20 md:px-20 sm:px-16 justify-end component">
       <div className="flex flex-col max-h-full xl:max-w-xl lg:max-w-md lg:justify-start md:justify-center sm:justify-center justify-center">
         <h1 className="xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-3xl lg:text-left text-center font-bold text-white">Sebastian Quintero</h1>
-        <h3 className="sm:mt-4 mt-1 lg:text-4xl md:text-3xl sm:text-2xl text-2xl lg:text-left text-center font-bold" style={secondColor}>Desarrollador FullStack</h3>
+        <h3 className="sm:mt-4 mt-1 lg:text-4xl md:text-3xl sm:text-2xl text-2xl lg:text-left text-center font-bold" style={secondColor}>Desarrollador Backend</h3>
         <p className="sm:text-xl text-lg lg:text-left text-center sm:mt-8 mt-5 text-white">Como 
-          desarrollador FullStack, traduzco conceptos creativos
-          en experiencias digitales excepcionales. Adéntrate en mi portafolio
-          para explorar proyectos únicos donde la magia de la tecnología cobra
-          vida.</p>
+          Como desarrollador Backend, traduzco conceptos creativos en soluciones lógicas y estructuras sólidas. Adéntrate en mi portafolio para explorar proyectos únicos donde la magia de la tecnología se construye desde la base y cobra vida detrás de cada pantalla.</p>
           <div className="gap-4 flex mt-6 pt-4 border-t-2 justify-end">
             <div className="img-container">
             <a href="https://github.com/QuinSDev" target="_blank">
